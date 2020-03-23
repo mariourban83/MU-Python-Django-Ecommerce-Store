bd7b4d815d6e945ba9f27776095a9637093acd2c
mariourban83 <mario.urban.83@gmail.com>
2020-03-23 02:58:07 +0000

    Create templates with Bs4 for catalog views, edit settings for serving images locally

--- eshop/settings.py
+++ eshop/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,3 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
