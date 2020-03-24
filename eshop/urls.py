a859bf4d40e84206af9344268a52231ec90c8dc0
mariourban83 <mario.urban.83@gmail.com>
2020-03-24 15:53:05 +0000

    Add urlpatterns for cart views

--- eshop/urls.py
+++ eshop/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
