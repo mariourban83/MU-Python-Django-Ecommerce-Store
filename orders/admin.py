b56f206d28f98314eb8db45302df4aaa43fdae01
mariourban83 <mario.urban.83@gmail.com>
2020-03-28 09:37:17 +0000

    Add Export to csv option functionality to admin area actions

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,30 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; '\
+        'filename={}.csv'.format(opts.verbose_name)
+    writter = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many
+              and not field.one_to_many]
+    writter.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writter.writerow(data_row)
+    return response
+    export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -14,3 +39,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
