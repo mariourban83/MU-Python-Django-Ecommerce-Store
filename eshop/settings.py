2c4e8e7fe87b08458e959e329b68dcb24cfa52c7
mariourban83 <mario.urban.83@gmail.com>
2020-03-30 22:16:00 +0100

    Add * to allowed hosts

--- eshop/settings.py
+++ eshop/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://lr-onlineshop.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
