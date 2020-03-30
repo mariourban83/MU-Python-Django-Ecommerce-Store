4f4c0cdfd24a503b5acf9df26bd6fc4e5f76a38d
mariourban83 <mario.urban.83@gmail.com>
2020-03-30 22:04:11 +0100

    Update allowed hosts for heroku

--- eshop/settings.py
+++ eshop/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'https://lr-onlineshop.herokuapp.com']
 
 
 # Application definition
