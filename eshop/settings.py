f0229dd3f2b22811d3e5a049a84f0dc634a88715
mariourban83 <mario.urban.83@gmail.com>
2020-04-01 02:07:01 +0100

    Add allowed hosts to settings

--- eshop/settings.py
+++ eshop/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/', 'https://lr-onlineshop.herokuapp.com']
 
 
 # Application definition
